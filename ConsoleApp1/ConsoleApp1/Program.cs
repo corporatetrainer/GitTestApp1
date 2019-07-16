95cbb6c1e4437647be6b98b3ad15b4bad9ece908
corporatetrainer <52920152+corporatetrainer@users.noreply.github.com>
2019-07-16 17:44:20 +0530

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // comment added from Git Hub
+            // comments added from Git Hub
         }
     }
 }
